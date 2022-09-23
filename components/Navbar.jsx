a65193a9eef5c55ea384b18f27c6d7fcc90ec180
Amchia Mohamed <amchiamohamed41@gmail.com>
2022-09-23 11:28:23 +0000

    first

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 export const Navbar = () => {
   const sytle = {
     anchor : "text-xl font-semibold mx-[15px]",
-    button : "ml-[0.5rem] px-[15px] py-1 rounded-[50px] font-semibold"
+    button : "ml-[0.5rem] px-[15px] py-1 rounded-[50px] font-semibold outline-none"
   }
   return (
-    <nav className='w-full flex justify-between items-center rounded '>
+    <nav className='w-full flex justify-between items-center rounded'>
         <Image src='/img/MuzicLogo.png' className='cursor-pointer' alt="test" width={'40px'} height={'40px'}/>
         <ul className='hidden md:flex'>
             <li><a href="#" className={sytle.anchor}>Home</a></li>
